ff81224d655b09d3ce3e1c7584e67b300686f53e
0421cjy <0421cjy@naver.com>
2019-06-10 14:45:59 +0900

    lambda test code adder

--- cplusplus/lambda.cpp
+++ cplusplus/lambda.cpp
@@ -1,6 +1,14 @@
 ﻿#include <iostream>
+#include <functional>
 
 int main()
 {
+    std::function<int(int)> factorial = [&factorial](int number) {
+        return number > 1 ? number * factorial(number - 1) : 1;
+    };
 
+    std::cout << factorial(5) << std::endl;
+
+    int in;
+    std::cin >> in;
 }
