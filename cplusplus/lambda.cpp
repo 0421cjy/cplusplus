776c3ae8a2f03805833a4790e0cab9f1e087e9df
heavybolt <cherryofsnow@gmail.com>
2019-09-09 17:14:43 +0900

    lambda file update

--- cplusplus/lambda.cpp
+++ cplusplus/lambda.cpp
@@ -1,13 +1,60 @@
 ﻿#include <iostream>
 #include <functional>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+
+template <typename T>
+void fill(std::vector<int>& vec, T done)
+{
+    int i = 0;
+
+    while (!done())
+    {
+        vec.push_back(i++);
+    }
+}
+
+class gorp
+{
+    std::vector<int> values;
+    int m;
+
+public:
+    gorp(int mod) : m(mod) {}
+    gorp& put(int v)
+    {
+        values.push_back(v);
+        return *this;
+    }
+
+    int extras()
+    {
+        int count = 0;
+        for_each(values.begin(), values.end(), [=, &count](int v) {count += v % m; });
+        return count;
+    }
+};
 
 int main()
 {
-    std::function<int(int)> factorial = [&factorial](int number) {
-        return number > 1 ? number * factorial(number - 1) : 1;
+    std::function<int(int)> factioral = [&](int number)
+    {
+        return number > 1 ? number *= factioral(number - 1) : 1;
     };
 
-    std::cout << factorial(5) << std::endl;
+    std::cout << factioral(5) << std::endl;
+
+    std::vector<int> stuff;
+    fill(stuff, [&]() { return stuff.size() >= 8; });
+
+    for_each(stuff.begin(), stuff.end(), [&](int i) { std::cout << i << " "; });
+
+    gorp g(4);
+    g.put(3).put(7).put(8);
+
+    std::cout << "extras: " << g.extras();
 
     int in;
     std::cin >> in;
