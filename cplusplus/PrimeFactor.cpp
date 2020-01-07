f7d5100ebc93554746de6e23df36b994dbd7512a
heavybolt <cherryofsnow@gmail.com>
2020-01-07 15:57:56 +0900

    primeFactor Function add

---
+++ cplusplus/PrimeFactor.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void FindPrimeFactor()
+{
+    while (true)
+    {
+        cout << "Find Prime Factor" << endl;
+        cout << "Please Insert Number..." << endl;
+
+        int number;
+        scanf_s("%d", &number);
+
+        if (number == 0) return;
+
+        for (int i = 2; i < number; i++)
+        {
+            while (number % i == 0)
+            {
+                number /= i;
+                printf_s("%d ", i);
+            }
+        }
+
+        printf("\n");
+        printf("---------------------------------------------------------");
+        printf("\n");
+    }
+}
